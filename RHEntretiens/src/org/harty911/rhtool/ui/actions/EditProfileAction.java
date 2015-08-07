27fc4a00b3b23c7b9ec65280e0bf789ec57e1499
P13 <P13@LW5P13CEM.dsone.3ds.com>
2015-08-07 17:31:36 +0200

    missing files

---
+++ RHEntretiens/src/org/harty911/rhtool/ui/actions/EditProfileAction.java
@@ -0,0 +1,39 @@
+package org.harty911.rhtool.ui.actions;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.harty911.rhtool.RHToolApp;
+import org.harty911.rhtool.core.model.RHModel;
+import org.harty911.rhtool.core.model.objects.User;
+import org.harty911.rhtool.ui.dialogs.UserFormDialog;
+import org.harty911.rhtool.ui.resources.Icons;
+import org.harty911.rhtool.ui.utils.UIModelUtils;
+
+public class EditProfileAction extends Action {
+
+	public EditProfileAction() {
+		super("&Profil utilisateur");
+		setImageDescriptor( Icons.getDescriptor(Icons.USER_ADMIN));
+	}
+
+	@Override
+	public void run() {
+		Shell shell = Display.getCurrent().getActiveShell();
+		RHModel model = RHToolApp.getModel();
+		User user = RHToolApp.getModel().getUserContext();
+
+		if( !UIModelUtils.refreshAndCheck(user)) {
+			MessageDialog.openError(shell, "Profil utilisateur", "Votre profil a été désactivé, seul l'administrateur peut le modifier !");
+			return;
+		}
+		
+		UserFormDialog dlg = new UserFormDialog( shell, user);
+		if( dlg.open()==Window.OK) {
+			model.save(user);
+			RHToolApp.getWindow().updateFromModel();
+		}
+	}
+}
