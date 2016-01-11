69abbcdf828e1841a3f337c3a0d040a65307fce9
harty911 <harty911@free.fr>
2016-01-11 17:49:51 +0100

    build V1.2_b51

--- RHEntretiens/src/org/harty911/rhtool/AppInfos.java
+++ RHEntretiens/src/org/harty911/rhtool/AppInfos.java
@@ -4,7 +4,7 @@ public class AppInfos {
 
 	public static final String ID = "org.harty911.rhtool";
 	public static final String APP_NAME = "Gestion des entretiens RH";
-	public static final String APP_VERSION = "1.1_b48";
+	public static final String APP_VERSION = "1.2_b51";
 	
 	public static final boolean TESTING = false;
 }
