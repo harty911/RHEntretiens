39db62af3594c1b28bac791c166f6b8cb765e90b
harty911 <harty911@free.fr>
2016-03-08 18:04:13 +0100

    ?

--- RHEntretiens/src/org/harty911/rhtool/core/utils/PPTStatsExporter.java
+++ RHEntretiens/src/org/harty911/rhtool/core/utils/PPTStatsExporter.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
-
+import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
 
